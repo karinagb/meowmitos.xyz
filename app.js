6e702a83466929f7cfddaf6bbf2d8447d4c3bb73
karinagb <karin.gb27@gmail.com>
2024-06-27 17:42:51 -0600

    Remove xyz

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const http = require("http");
 
 http
   .createServer(function (req, res) {
-    res.write("Welcome to Meowmitos.xyz!");
+    res.write("Welcome to Meowmitos!");
     res.end();
   })
-  .listen(3000);
+  .listen(3001);
 
 console.log("Server started on port 3000");
